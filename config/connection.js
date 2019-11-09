671ac432fc129fd68d7f08489749bb96f93a2abc
Nick Frattaroli <Nafratt@gmail.com>
2019-11-09 12:54:31 -0500

    updateing connection.js with orm

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// add mySQL
+const mysql = require('promise-mysql');
+
+// ORM's Export connection 
+module.exports = {
+    create: async function() {
+      try {
+        this.connection = await mysql.createConnection(require('./db-config'));
+        console.log('DATABASE CONNECTION ESTABLISHED');
+        console.table(this.connection.config);
+      } catch (error) {
+        console.log('ERROR: DB CONNECTION FAILED');
+        console.table(error);
+        process.exit(1);
+      }
+    },
+    get: function() {
+      return this.connection;
+    },
+    connection: null,
+  };
+  
