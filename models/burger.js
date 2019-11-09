1c463eadca20f29799e6f8333899ef0c9b34fecd
Nick Frattaroli <Nafratt@gmail.com>
2019-11-09 13:59:40 -0500

    setting up orm in burger js

---
+++ models/burger.js
@@ -0,0 +1,16 @@
+const orm = require('../config/orm.js')
+
+const burger={
+    all:function(){
+    return orm.all('burgers');;
+},
+create: function(cols,vals){
+    return orm.create('burgers',cols,vals);
+},
+create: function(objcolvals,condition){;
+    return orm.update('burgers',objcolvals,condition);
+},
+delete:function(condition){
+    return orm.delete('burgers',condition)
+}
+};
