f9c7f97917f5651da70fafea82018a10113d734b
Nick Frattaroli <Nafratt@gmail.com>
2019-11-14 22:14:52 -0500

    updating connection and index

--- config/connection.js
+++ config/connection.js
@@ -3,20 +3,19 @@ const mysql = require('promise-mysql');
 
 // ORM's Export connection 
 module.exports = {
-    create: async function() {
-      try {
-        this.connection = await mysql.createConnection(require('./db-config'));
-        console.log('DATABASE CONNECTION ESTABLISHED');
-        console.table(this.connection.config);
-      } catch (error) {
-        console.log('ERROR: DB CONNECTION FAILED');
-        console.table(error);
-        process.exit(1);
-      }
-    },
-    get: function() {
-      return this.connection;
-    },
-    connection: null,
-  };
-  
+  create: async function () {
+    try {
+      this.connection = await mysql.createConnection(process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : require('./db-config'));
+      console.log('DATABASE CONNECTION ESTABLISHED');
+      console.table(this.connection.config);
+    } catch (error) {
+      console.log('ERROR: DB CONNECTION FAILED');
+      console.table(error);
+      process.exit(1);
+    }
+  },
+  get: function () {
+    return this.connection;
+  },
+  connection: null,
+};
