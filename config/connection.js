4042ef0314d01d6945e204ad52e3104bd6d1d36a
Nick Frattaroli <Nafratt@gmail.com>
2019-11-21 22:01:32 -0500

    new config

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 // add mySQL
 const mysql = require('promise-mysql');
+const dbconfig=require('db-config.js')
 
 // ORM's Export connection 
 module.exports = {
   create: async function () {
     try {
       console.log(process.env.NODE_ENV === 'production',process.env.JAWSDB_URL);
-      this.connection = await mysql.createConnection(process.env.JAWSDB_URL );
+      this.connection = await mysql.createConnection(dbconfig );
       console.log('DATABASE CONNECTION ESTABLISHED');
       console.table(this.connection.config);
     } catch (error) {
